8d21ba2719c2258fc5e919ab68cebf30dfc561e2
hong-il <clroyan01@gmail.com>
2020-11-02 00:04:24 +0900

    Create post page - Initial commit

--- app.py
+++ app.py
@@ -8,15 +8,28 @@
     @ Date      : 2020-11-01
     @ File name : app.py
     @ File path : /
-    @ Description : Rendering page
+    @ Description : Rendering, request page
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort, redirect, url_for
 from flask_pymongo import PyMongo
-
+from datetime import datetime
+from bson.objectid import ObjectId
+import time
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/crud"
 mongo = PyMongo(app)
+
+
+@app.template_filter("datetime_format")
+def datetime_format(value):
+    if value is None:
+        return ""
+
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    value = datetime.fromtimestamp((int(value) / 1000)) + offset
+    return value.strftime("%B %d, %Y")
 
 
 @app.route('/')
@@ -34,22 +47,42 @@ def write():
         PST_CAT_TXT = request.form.get("PST_CAT_TXT")
         PST_CONTENT = request.form.get("PST_CONTENT")
 
+        PST_CREATED_DATE = round(datetime.utcnow().timestamp() * 1000)
         PST_MST = mongo.db.post
-        post = {
+        insert_post = {
             "PST_CAT_TP": PST_CAT_TP,
             "PST_TITLE": PST_TITLE,
             "PST_SUB_TITLE": PST_SUB_TITLE,
             "PST_CATEGORY": PST_CAT_TXT if PST_CATEGORY == 0 else PST_CATEGORY,
-            "PST_CONTENT": PST_CONTENT
+            "PST_CONTENT": PST_CONTENT,
+            "PST_CREATED_DATE": PST_CREATED_DATE
         }
 
-        insertedPost = PST_MST.insert_one(post)
+        insertedPost = PST_MST.insert_one(insert_post)
         inserted_id = str(insertedPost.inserted_id)
-        print(inserted_id)
 
-        return render_template('index.html')
+        return redirect(url_for("post", PST_ID=inserted_id))
     else:
-        return render_template('write.html')
+        return render_template("write.html")
 
 
+@app.route('/post/<PST_ID>')
+def post(PST_ID):
+    # PST_ID = request.args.get("PST_ID")
+    if PST_ID is not None:
+        PST_MST = mongo.db.post
+        data = PST_MST.find_one({"_id": ObjectId(PST_ID)})
 
+        if data is not None:
+            select_post = {
+                "PST_ID": data.get("_id"),
+                "PST_CAT_TP": data.get("PST_CAT_TP"),
+                "PST_TITLE": data.get("PST_TITLE"),
+                "PST_SUB_TITLE": data.get("PST_SUB_TITLE"),
+                "PST_CATEGORY": data.get("PST_CATEGORY"),
+                "PST_CONTENT": data.get("PST_CONTENT"),
+                "PST_CREATED_DATE": data.get("PST_CREATED_DATE")
+            }
+
+            return render_template("post.html", post=select_post)
+    return abort(404)
